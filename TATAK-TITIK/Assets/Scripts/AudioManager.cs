ec255e8b9e1d384759e860c1f9d12ec856860809
madscale-xd <justinkyledecastro@gmail.com>
2025-07-26 17:47:29 +0800

    Audio setup

---
+++ TATAK-TITIK/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Audio Sources")]
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    [Header("BGM")]
+    public AudioClip mainMenuBGM;
+    public AudioClip gameBGM;
+    public AudioClip gameOverBGM;
+
+    [Header("SFX")]
+
+    public AudioClip buttonSFX;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            sfxSource.ignoreListenerPause = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu")
+            PlayBGM(mainMenuBGM);
+        else if (scene.name == "TestScene")
+            PlayBGM(gameBGM);
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        if (bgmSource.clip == clip) return;
+        bgmSource.clip = clip;
+        bgmSource.loop = true;
+        bgmSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    // Separated SFX methods
+}
